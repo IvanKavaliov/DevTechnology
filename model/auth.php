01acc8f2b2e3845a587597336c30069f94310a19
IvanKavaliov <ivan.kavaliov@gmail.com>
2022-02-21 20:21:40 +0300

    feat: add function checkAuth

--- model/auth.php
+++ model/auth.php
@@ -1,12 +1,25 @@
 <?php
-    $login = $_POST['login'];
-    $password = $_POST['password'];
-    $userData = [
-        "login" => 'Jack',
-        "password" => '12345'
-    ];
-    if ($login === $userData['login'] && $password === $userData['password']) {
-        echo "Good, right password";
-    } else {
-        require_once "../View/forms/authForm.php";
+
+    class Authorization {
+        private $login;
+        private $password;
+
+        public function __construct($userLogin, $userPassword)
+        {
+            $this->login = $userLogin;
+            $this->password = $userPassword;
+        }
+
+        public function checkAuth($userLogin, $userPassword)
+        {
+            $userData = require_once "adminData.php";
+            if ($userLogin === $userData['login'] && $userPassword === $userData['password']) {
+                require_once "../View/adminPanel.php";
+            } else {
+                require_once "../View/forms/authForm.php";
+            }
+        }
     }
+
+    $adminAuth = new Authorization($_POST['login'], $_POST['password']);
+    $adminAuth->checkAuth($_POST['login'], $_POST['password']);
