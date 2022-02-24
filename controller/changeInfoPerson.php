0c52d1576dae2702bd400323346a9d23c1a8364f
IvanKavaliov <ivan.kavaliov@gmail.com>
2022-02-24 16:45:25 +0300

    feat: add function deletePersonImage

--- controller/changeInfoPerson.php
+++ controller/changeInfoPerson.php
@@ -1,18 +1,17 @@
 <?php
     require_once '../model/classPerson.php';
     $idPerson = $_POST['saveButton'];
+    require_once "../model/connectionToDatabase.php";
     if (@$_FILES['image']['tmp_name'][0]) {
         require_once '../model/uploadImage.php';
         $fileName = uploadImage($_FILES['image']);
-        require_once "../model/connectionToDatabase.php";
         $query = "SELECT image FROM team WHERE id = $idPerson";
         $result = mysqli_query($connection, $query);
         $team = mysqli_fetch_assoc($result);
         $fileNameDelete = $team['image'];
-        $filePath = "../images/personImages/$fileNameDelete";
-        unlink($filePath);
+        require_once '../model/deletePersonImage.php';
+        deletePersonImage($fileNameDelete);
     } else {
-        require_once "../model/connectionToDatabase.php";
         $query = "SELECT image FROM team WHERE id = $idPerson";
         $result = mysqli_query($connection, $query);
         $team = mysqli_fetch_assoc($result);
@@ -22,7 +21,6 @@
                                 htmlspecialchars($_POST['info']), htmlspecialchars($_POST['link_facebook']), 
                                 htmlspecialchars($_POST['link_twitter']), htmlspecialchars($_POST['link_git']), 
                                 htmlspecialchars($_POST['link_email']), $fileName);
-    require_once "../model/connectionToDatabase.php"; 
     $query = "UPDATE team SET `name` = '$newInfoPerson->name', `position` = '$newInfoPerson->position', `info` = '$newInfoPerson->info', `link_facebook` = '$newInfoPerson->link_facebook', `link_twitter` = '$newInfoPerson->link_twitter', `link_git` = '$newInfoPerson->link_git', `link_email` = '$newInfoPerson->link_email', `image` = '$newInfoPerson->person_image' WHERE id = $idPerson";
     $result = mysqli_query($connection, $query)
     or die ('Error in query to database');
--- controller/deletePerson.php
+++ controller/deletePerson.php
@@ -1,8 +1,14 @@
 <?php  
     require_once "../model/connectionToDatabase.php";
-    $id_delete_person = $_POST['deleteSubmit'];
+    $idDeletePerson = $_POST['deleteSubmit'];
+    $query = "SELECT image FROM team WHERE id = $idDeletePerson";
+    $result = mysqli_query($connection, $query);
+    $team = mysqli_fetch_assoc($result);
+    $fileNameDelete = $team['image'];
+    require_once '../model/deletePersonImage.php';
+    deletePersonImage($fileNameDelete);
     require_once "../model/queries.php";
-    deletePersonQuery($id_delete_person, $connection);
+    deletePersonQuery($idDeletePerson, $connection);
     mysqli_close($connection);
     header('location: ../View/adminPanel.php');
     exit();
