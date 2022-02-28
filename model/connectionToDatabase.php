9283c96106d15e70bf2f6cc312d1902419405542
IvanKavaliov <ivan.kavaliov@gmail.com>
2022-02-28 17:00:19 +0300

    feat: add connectionDB.php in gitignore

--- model/connectionToDatabase.php
+++ model/connectionToDatabase.php
@@ -1,9 +1,10 @@
 <?php
-    $id = '127.0.0.1';
-    $name_DB = 'root';
-    $password = 'root';
-    $db = 'small_team';
-    $connection = mysqli_connect($id, $name_DB, $password, $db);
+    // $id = '127.0.0.1';
+    // $name_DB = 'root';
+    // $password = 'root';
+    // $db = 'small_team';
+    $connectionDB = require_once 'connectionDB.php';
+    $connection = mysqli_connect($connectionDB['id'], $connectionDB['name_DB'], $connectionDB['password'], $connectionDB['db']);
     if ($connection == false)
     {
         echo 'Failed to connect to the database!<br>';
