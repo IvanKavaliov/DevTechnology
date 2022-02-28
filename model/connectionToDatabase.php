44a6666fa78564dd9221c519ff77cbdf0b474fa2
IvanKavaliov <ivan.kavaliov@gmail.com>
2022-02-28 17:01:36 +0300

    feat: delete config date

--- model/connectionToDatabase.php
+++ model/connectionToDatabase.php
@@ -1,8 +1,4 @@
 <?php
-    // $id = '127.0.0.1';
-    // $name_DB = 'root';
-    // $password = 'root';
-    // $db = 'small_team';
     $connectionDB = require_once 'connectionDB.php';
     $connection = mysqli_connect($connectionDB['id'], $connectionDB['name_DB'], $connectionDB['password'], $connectionDB['db']);
     if ($connection == false)
