2219324418d697b2abfe74f8baefe0c1afcf197c
IvanKavaliov <ivan.kavaliov@gmail.com>
2022-03-16 17:33:09 +0300

    refactor: delete comments

--- model/personTemplate.php
+++ model/personTemplate.php
@@ -1,6 +1,5 @@
 <div class="container mb-3 col-11">
     <div class="row col-sm-9 col-md-12 col-lg-6 col-xl-5 pt-3 justify-content-between rounded border">
-    <!-- style="height: 50px; width:500px; padding-top: 13px;" -->
         <div class="d-flex flex-row justify-content-between">
             <div class="block col-9 col-md-10">
                 <p><?php echo $name." - ".$position ?></p>
